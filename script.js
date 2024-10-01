67212e89bb23a1acdda216119aa67b3832a8c2ea
Eric-Fang-ICD201-01 <eric.fang@student.tdsb.on.ca>
2024-10-01 20:50:09 +0000

    commit

--- script.js
+++
@@ -1,7 +0,0 @@
-var clicks = 0
-
-function talk() {
-    var speech = document.getElementById("speech")
-    clicks++
-    speech.innerHTML = "Bob says hi! You have clicked Bob " + clicks + " times!"
-}
